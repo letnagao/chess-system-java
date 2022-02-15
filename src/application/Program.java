ef9276c0eb357fc6e6f197eb5cbe608669c93697
Leticia Lumi <leticia-nagao@hotmail.com>
2022-02-15 16:58:51 -0300

    Little improvement in board printing

--- src/application/Program.java
+++ src/application/Program.java
@@ -5,7 +5,9 @@ import chess.ChessMatch;
 public class Program {
 
 	public static void main(String[] args) {
-
+		
+		//Caminho pasta chess-system > bin > git bash here > (comando) java application/Program
+		
 		ChessMatch chessMatch = new ChessMatch();
 		UI.printBoard(chessMatch.getPieces());
 	}
