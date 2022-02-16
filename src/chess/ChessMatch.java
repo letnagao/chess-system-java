a834dfa1c3efc7ef71994a4717a92fc00b7d38d2
Leticia Lumi <leticia-nagao@hotmail.com>
2022-02-16 17:44:30 -0300

    Pawn

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -8,6 +8,7 @@ import boardgame.Board;
 import boardgame.Piece;
 import boardgame.Position;
 import chess.pieces.King;
+import chess.pieces.Pawn;
 import chess.pieces.Rook;
 
 public class ChessMatch {
@@ -90,8 +91,8 @@ public class ChessMatch {
 		board.placePiece(p, target);
 		
 		if(capturedPiece != null) {
-			piecesOnTheBoard.remove(capturedPiece);
 			capturedPieces.add(capturedPiece);
+			piecesOnTheBoard.remove(capturedPiece);
 		}
 		
 		return capturedPiece;
@@ -123,7 +124,7 @@ public class ChessMatch {
 	
 	private void validateTargetPosition(Position source, Position target) {
 		if (!board.piece(source).possibleMove(target)) {
-			throw new ChessException("The chosen piece can't move to target position.");
+			throw new ChessException("The chosen piece can't move to target position");
 		}
 	}
 	
@@ -133,7 +134,7 @@ public class ChessMatch {
 	}
 	
 	private Color opponent(Color color) {
-		return(color == Color.WHITE) ? Color.BLACK : Color.WHITE;
+		return (color == Color.WHITE) ? Color.BLACK : Color.WHITE;
 	}
 	
 	private ChessPiece king(Color color) {
@@ -189,11 +190,28 @@ public class ChessMatch {
 	}
 	
 	private void initialSetup() {
-		placeNewPiece('h', 7, new Rook(board, Color.WHITE));
-		placeNewPiece('d', 1, new Rook(board, Color.WHITE));
-		placeNewPiece('e', 1, new King(board, Color.WHITE));
-		
-		placeNewPiece('b', 8, new Rook(board, Color.BLACK));
-		placeNewPiece('a', 8, new King(board, Color.BLACK));
+		 	placeNewPiece('a', 1, new Rook(board, Color.WHITE));
+	        placeNewPiece('e', 1, new King(board, Color.WHITE));
+	        placeNewPiece('h', 1, new Rook(board, Color.WHITE));
+	        placeNewPiece('a', 2, new Pawn(board, Color.WHITE));
+	        placeNewPiece('b', 2, new Pawn(board, Color.WHITE));
+	        placeNewPiece('c', 2, new Pawn(board, Color.WHITE));
+	        placeNewPiece('d', 2, new Pawn(board, Color.WHITE));
+	        placeNewPiece('e', 2, new Pawn(board, Color.WHITE));
+	        placeNewPiece('f', 2, new Pawn(board, Color.WHITE));
+	        placeNewPiece('g', 2, new Pawn(board, Color.WHITE));
+	        placeNewPiece('h', 2, new Pawn(board, Color.WHITE));
+
+		 	placeNewPiece('a', 8, new Rook(board,Color.BLACK));
+	        placeNewPiece('e', 8, new King(board,Color.BLACK));
+	        placeNewPiece('h', 8, new Rook(board,Color.BLACK));
+	        placeNewPiece('a', 7, new Pawn(board,Color.BLACK));
+	        placeNewPiece('b', 7, new Pawn(board,Color.BLACK));
+	        placeNewPiece('c', 7, new Pawn(board,Color.BLACK));
+	        placeNewPiece('d', 7, new Pawn(board,Color.BLACK));
+	        placeNewPiece('e', 7, new Pawn(board,Color.BLACK));
+	        placeNewPiece('f', 7, new Pawn(board,Color.BLACK));
+	        placeNewPiece('g', 7, new Pawn(board,Color.BLACK));
+	        placeNewPiece('h', 7, new Pawn(board,Color.BLACK));
 	}
 }
--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -5,10 +5,10 @@ import boardgame.Piece;
 import boardgame.Position;
 
 public abstract class ChessPiece extends Piece {
-	
+
 	private Color color;
 	private int moveCount;
-	
+
 	public ChessPiece(Board board, Color color) {
 		super(board);
 		this.color = color;
@@ -22,20 +22,20 @@ public abstract class ChessPiece extends Piece {
 		return moveCount;
 	}
 	
-	public void increaseMoveCount() {
+	protected void increaseMoveCount() {
 		moveCount++;
 	}
-	
-	public void decreaseMoveCount() {
+
+	protected void decreaseMoveCount() {
 		moveCount--;
 	}
-	
+
 	public ChessPosition getChessPosition() {
 		return ChessPosition.fromPosition(position);
 	}
 	
 	protected boolean isThereOpponentPiece(Position position) {
 		ChessPiece p = (ChessPiece)getBoard().piece(position);
-		return p != null & p.getColor() != color;
+		return p != null && p.getColor() != color;
 	}
 }
